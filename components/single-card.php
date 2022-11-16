ac6ac364341915d23fb69fe25ed706b08d498aa7
Mike Dabiem <mikedabiem@gmail.com>
2022-11-16 18:48:57 +0200

    backup

--- components/single-card.php
+++ components/single-card.php
@@ -3,7 +3,7 @@
         <img src="<?php echo $card["image"]; ?>" alt="sneaker">
     </a>
     <a href="#" class="item__title"><?php echo $card["name"]; ?></a>
-    <p class="item__price">$<?php echo $card["price"]; ?></p>
+    <p class="item__price">$<?php echo number_format($card["price"], 2, '.', ' '); ?></p>
     <div class="item__buttons">
         <a href="#" class="item__buttons-view">View item</a>
         <input class="item__buttons-heart" type="checkbox" name="heart" <?php echo $card["liked"] ? "checked" : null ?>>
